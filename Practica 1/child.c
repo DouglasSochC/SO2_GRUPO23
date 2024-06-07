cfce7d139668e8bbf6c210ec55772e90e6013572
DouglasSochC <3008474750101@ingenieria.usac.edu.gt>
2024-06-07 07:57:28 -0600

    [UPDATE] Se ha agregado la separacion de codigo para la parte del padre y del hijo

---
+++ Practica 1/child.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[])
+{
+    // printf("Soy el proceso hijo\n");
+    // printf("Mi PID es: %d\n", getpid());
+
+    printf("Argumento 1: %s\n", argv[1]);
+    printf("Argumento 2: %s\n", argv[2]);
+
+    sleep(4);
+
+    // Exit code
+    exit(1);
+}
--- Practica 1/parent.c
+++ Practica 1/parent.c
@@ -10,12 +10,41 @@ int main()
 
     pid_t pid = fork();
 
+    if (pid == -1)
+    {
+        perror("fork");
+        exit(1);
+    }
+
     if (pid)
     {
-        printf("Soy el proceso padre \n");
+        // Proceso padre
+        printf("Soy el proceso padre - %d\n", pid);
+        printf("Mi PID es: %d\n\n", getpid());
+
+        int status;
+        wait(&status);
+
+        if (WIFEXITED(status))
+        {
+            printf("\nEl proceso hijo termino con estado: %d\n", status);
+        }
+        else
+        {
+            printf("\nError: El proceso hijo termino con estado: %d\n", status);
+        }
+
+        printf("Terminando el proceso padre\n");
     }
     else
     {
-        printf("Soy el proceso hijo \n");
+        // Proceso hijo
+        char *arg_Ptr[4];
+        arg_Ptr[0] = " child.bin";
+        arg_Ptr[1] = " Hola";
+        arg_Ptr[2] = " Soy el proceso hijo! ";
+        arg_Ptr[3] = NULL;
+
+        execv("./child.bin", arg_Ptr);
     }
 }
