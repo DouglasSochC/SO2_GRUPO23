88f4a5141a28ef2862c731acfac73a16c6438a08
DouglasSochC <3008474750101@ingenieria.usac.edu.gt>
2024-06-07 10:55:29 -0600

    [ADD] Manejador del SIGINT

--- Practica1/parent.c
+++ Practica1/parent.c
@@ -1,25 +1,52 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <string.h>
 #include <sys/wait.h>
 
+#define LOG_FILE "syscalls.log"
+#define FILE_NAME "practica1.txt"
+
+// Variables globales para contar las llamadas
+int total_system_calls = 0;
+int total_read_calls = 0;
+int total_write_calls = 0;
+int total_open_calls = 0;
+
+// Función para manejar la senial del SIGINT (Ctrl + C)
+void handle_sigint(int signum)
+{
+    printf("\n**********************\n");
+    printf("\nSeñal SIGINT recibida\n");
+    printf("\n**********************\n");
+    printf("Número total de llamadas al sistema: %d\n", total_system_calls);
+    printf("Número de llamadas al sistema por tipo (Read: %d, Write: %d, Open: %d)\n", total_read_calls, total_write_calls, total_open_calls);
+    exit(0);
+}
+
 int main()
 {
+
+    // Manejador del SIGINT
+    signal(SIGINT, handle_sigint);
+
+    // Crear o vaciar el archivo de los logs
+    FILE *log_file = fopen(LOG_FILE, "w");
+    fclose(log_file);
 
     pid_t pid = fork();
 
     if (pid == -1)
     {
-        perror("\nError al crear el PID\n");
+        perror("fork");
         exit(1);
     }
-
-    if (pid)
+    else if (pid)
     {
         // Proceso padre
-        printf("Soy el proceso padre y tengo un hijo con PID: %d\n", pid);
+        printf("Soy el proceso padre - %d\n", pid);
         printf("Mi PID es: %d\n\n", getpid());
 
         int status;
@@ -41,7 +68,7 @@ int main()
         // Proceso hijo
         char *arg_Ptr[4];
         arg_Ptr[0] = " child.bin";
-        arg_Ptr[1] = " Mensaje 1 desde el hijo";
+        arg_Ptr[1] = " Hola";
         arg_Ptr[2] = " Soy el proceso hijo! ";
         arg_Ptr[3] = NULL;
 
