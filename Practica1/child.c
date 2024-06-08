b14bdb25646933e201ced4ed5ff7c78e76ad47c6
Carlos Martinez <2730538920101@ingenieria.usac.edu.gt>
2024-06-07 22:28:54 -0600

    [ADD] Read y Write de caracteres aleatorios en txt

--- Practica1/child.c
+++ Practica1/child.c
@@ -3,14 +3,25 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <string.h>
-#include <sys/wait.h>
+#include <time.h>
+
+// Prototipos de funciones
+void generar_y_guardar_caracteres(const char *nombre_archivo);
+void leer_caracteres(const char *nombre_archivo);
 
 int main(int argc, char *argv[])
 {
+    if (argc < 2) {
+        fprintf(stderr, "Uso: %s <nombre_archivo>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
-    // printf("Soy el proceso hijo \n");
+    const char *nombre_archivo = argv[1];
+
+    printf("El archivo de escritura aleatoria es: %s\n", nombre_archivo);
     printf("Mi PID es: %d\n", getpid());
-    // printf("Argumento 1: %s\n", argv[1]);
+
+    srand(time(NULL)); // Inicializa el generador de números aleatorios
 
     while (1)
     {
@@ -19,16 +30,59 @@ int main(int argc, char *argv[])
         {
             case 0:
             {
+                generar_y_guardar_caracteres(nombre_archivo);
                 printf("%d - W\n", getpid());
                 break;
             }
             case 1:
             {
+                leer_caracteres(nombre_archivo);
                 printf("%d - R\n", getpid());
                 break;
             }
         }
-        int tiempo_espera = rand() % 2 + 1;
+        int tiempo_espera = rand() % 3 + 1;
         sleep(tiempo_espera);
     }
+
+    return 0;
+}
+
+// Función para generar y guardar 8 caracteres aleatorios en el archivo
+void generar_y_guardar_caracteres(const char *nombre_archivo) {
+    FILE *archivo;
+    archivo = fopen(nombre_archivo, "w"); // Modo append para añadir al final
+
+    if (archivo == NULL) {
+        perror("Error al abrir el archivo para escribir");
+        return;
+    }
+
+    for (int i = 0; i < 8; i++) {
+        char caracter_aleatorio = 'A' + (rand() % 26); // Genera un carácter aleatorio entre 'A' y 'Z'
+        fputc(caracter_aleatorio, archivo);
+    }
+
+    fclose(archivo);
+}
+
+// Función para leer y mostrar los primeros 8 caracteres del archivo
+void leer_caracteres(const char *nombre_archivo) {
+    FILE *archivo = fopen(nombre_archivo, "r");
+    if (archivo == NULL) {
+        perror("Error al abrir el archivo para leer");
+        return;
+    }
+
+    char buffer[9]; // Buffer para almacenar los caracteres leídos (8 caracteres + terminador nulo)
+    size_t leidos = fread(buffer, sizeof(char), 8, archivo);
+    buffer[leidos] = '\0'; // Añadir el terminador nulo al final
+
+    if (leidos > 0) {
+        printf("Los primeros 8 caracteres leídos son: %s\n", buffer);
+    } else {
+        printf("No se pudieron leer caracteres del archivo\n");
+    }
+
+    fclose(archivo);
 }
--- Practica1/parent.c
+++ Practica1/parent.c
@@ -15,19 +15,25 @@ int total_system_calls = 0;
 int total_read_calls = 0;
 int total_write_calls = 0;
 
+// Declaraciones de funciones
+void crear_archivo(const char *nombre_archivo);
+void eliminar_archivo(const char *nombre_archivo);
+void handle_sigint(int signum);
 
-// Función para manejar la senial del SIGINT (Ctrl + C)
+// Función para manejar la señal del SIGINT (Ctrl + C)
 void handle_sigint(int signum)
 {
+    
+    // Obtener el SIGINT y parar el programa
     printf("\n**********************\n");
-    printf("\nSeñal SIGINT recibida\n");
-    printf("\n**********************\n");
+    printf("Señal SIGINT recibida\n");
+    printf("**********************\n");
     printf("Número total de llamadas al sistema: %d\n", total_system_calls);
     printf("Número de llamadas al sistema por tipo (Read: %d, Write: %d)\n", total_read_calls, total_write_calls);
     exit(0);
 }
 
-// Funcion para crear y ejecutar un proceso hijo
+// Función para crear y ejecutar un proceso hijo
 pid_t create_and_exec_child(const char *program)
 {
     pid_t pid = fork();
@@ -41,9 +47,10 @@ pid_t create_and_exec_child(const char *program)
     // Ejecución del hijo
     else if (pid == 0)
     {
-        char *arg_Ptr[2];
+        char *arg_Ptr[3];
         arg_Ptr[0] = (char *)program;
-        arg_Ptr[1] = NULL;
+        arg_Ptr[1] = FILE_NAME;
+        arg_Ptr[2] = NULL;
         execv(program, arg_Ptr);
 
         // Si execv falla
@@ -56,6 +63,10 @@ pid_t create_and_exec_child(const char *program)
 
 int main()
 {
+    // Eliminar archivo txt
+    eliminar_archivo(FILE_NAME);
+    // Crear txt
+    crear_archivo(FILE_NAME);
 
     // Manejador del SIGINT
     signal(SIGINT, handle_sigint);
@@ -64,27 +75,27 @@ int main()
     FILE *log_file = fopen(LOG_FILE, "w");
     fclose(log_file);
 
-    // Creacion del hijo 1
+    // Creación del hijo 1
     pid_t pid_1 = create_and_exec_child("child.bin");
 
     // Se realiza una espera para que cada proceso tenga acciones diferentes
     sleep(1);
 
-    // Creacion del hijo 2
+    // Creación del hijo 2
     pid_t pid_2 = create_and_exec_child("child.bin");
 
-    // Variables auxiliares para ejecutar y obtener la informacion del trace
+    // Variables auxiliares para ejecutar y obtener la información del trace
     char command[100];
     char buffer[128];
     FILE *fp;
 
-    // Variable auxiliar para almacenar los log
+    // Variable auxiliar para almacenar los logs
     FILE *log_fp;
 
-    // Variable que nos ayuda a identificar cual es el tipo de operacion que se esta realizando
+    // Variable que nos ayuda a identificar cuál es el tipo de operación que se está realizando
     char primer_caracter;
 
-    // Comando para obtener la informacion del trace
+    // Comando para obtener la información del trace
     sprintf(command, "sudo stap %s %d %d", SCRIPT_NAME, pid_1, pid_2);
 
     // Abrir el comando para lectura
@@ -109,7 +120,7 @@ int main()
         // Obtener el primer caracter
         primer_caracter = buffer[0];
 
-        // Identificacion del tipo de operacion
+        // Identificación del tipo de operación
         if (primer_caracter == 'W')
         {
             total_write_calls++;
@@ -141,4 +152,32 @@ int main()
     int status;
     waitpid(pid_1, &status, 0);
     waitpid(pid_2, &status, 0);
+
+    return 0;
+}
+
+// Función para crear un archivo en la ruta actual
+void crear_archivo(const char *nombre_archivo) {
+    FILE *archivo;
+    archivo = fopen(nombre_archivo, "w");
+
+    if (archivo == NULL) {
+        perror("Error al crear el archivo");
+        return;
+    }
+
+    // Cierra el archivo después de crearlo
+    fclose(archivo);
+    
+    printf("Archivo '%s' creado en la ruta actual.\n", nombre_archivo);
+}
+
+// Función para eliminar un archivo
+void eliminar_archivo(const char *nombre_archivo) {
+    // Intenta eliminar el archivo
+    if (remove(nombre_archivo) == 0) {
+        printf("Archivo '%s' eliminado exitosamente.\n", nombre_archivo);
+    } else {
+        perror("Error al eliminar el archivo");
+    }
 }
