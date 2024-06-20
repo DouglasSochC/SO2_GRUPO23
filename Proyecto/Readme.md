a98963aab3e225e85c657aa8b8b90b884b85666f
Carlos Martinez <2730538920101@ingenieria.usac.edu.gt>
2024-06-20 12:02:40 -0600

    [ADD] Reader iniciado con conexion exitosa a mysql, se mejora el main.c

--- Proyecto/Readme.md
+++ Proyecto/Readme.md
@@ -2,3 +2,14 @@
 
 Usar el comando: sudo stap -v -DDEBUG memory_tracer.stp
 
+#   Para instalar la libreria del cliente de Mysql 
+
+Usar los comandos:
+
+-   sudo apt update
+-   sudo apt install libmysqlclient-dev
+
+#   Para compilar el archivo reader.c
+
+Usar el comando: gcc -o reader reader.c -lmysqlclient
+
--- Proyecto/generador/kernel/main.c
+++ Proyecto/generador/kernel/main.c
@@ -3,12 +3,25 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 
 #define LOG_FILE "../logs/memory_tracer.log"
 #define SYSTEMTAP_SCRIPT "../systemtap/memory_tracer.stp"
+#define LOG_FOLDER "../logs"
+
+// Función para crear la carpeta si no existe
+void create_log_folder() {
+    struct stat st = {0};
+
+    if (stat(LOG_FOLDER, &st) == -1) {
+        mkdir(LOG_FOLDER, 0755);
+    }
+}
 
 // Función para crear el archivo de log
 void create_log_file() {
+    create_log_folder();
+
     int fd = open(LOG_FILE, O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if (fd == -1) {
         perror("Error al crear el archivo de log");
@@ -31,6 +44,9 @@ void execute_systemtap_script() {
 
 // Función principal
 int main() {
+    // Crear la carpeta de logs si no existe
+    create_log_folder();
+
     // Crear el archivo de log
     create_log_file();
     printf("Archivo de log creado: %s\n", LOG_FILE);
